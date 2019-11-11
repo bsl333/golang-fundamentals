dc46001fdb80df470edcf39e31c7b87ceed34813
Branden <b.lowe333@gmail.com>
2019-11-11 11:59:18 -0800

    Example 2 on method sets complete

--- section12-concurrency/exercises/jedi-level-9.go
+++ section12-concurrency/exercises/jedi-level-9.go
@@ -9,6 +9,10 @@ import (
 func main() {
 	fmt.Println("Ex 1: Launch two new go routines and use wait group for them to finish and return results")
 	example1()
+
+	fmt.Println("Ex 2: method sets")
+	fmt.Print("\t")
+	example2()
 }
 
 func example1() {
@@ -28,4 +32,37 @@ func example1() {
 		}()
 	}
 	wg.Wait()
+}
+
+type person struct {
+	name string
+}
+
+func (p *person) speak(message string) {
+	fmt.Println("I,", p.name, ", said", message)
+}
+
+type human interface {
+	speak(message string)
+}
+
+func saySomething(h human) {
+	h.speak("This shall work")
+}
+
+func example2() {
+	/******
+	* Ex 2: Method sets
+	* create a type person struct
+	* attach a method speak to type person using a pointer receiver
+	* create a type human interface
+	* create func “saySomething”
+	* show the following in your code
+	*    - you CAN pass a value of type *person into saySomething
+	*    - you CANNOT pass a value of type person into saySomething
+	******/
+	p := person{"Branden"}
+	// saySomething(p) // won't work, as saySomething expects the pointer to p.
+	saySomething(&p)
+
 }

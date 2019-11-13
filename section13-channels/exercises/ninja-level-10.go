2c24371d7c4a215e028787612fb9227071321e3f
Branden <b.lowe333@gmail.com>
2019-11-12 17:15:47 -0800

    fix code and pull values off channel with range clause

--- section13-channels/exercises/ninja-level-10.go
+++ section13-channels/exercises/ninja-level-10.go
@@ -5,6 +5,7 @@ import "fmt"
 func main() {
 	exercise1()
 	exercise2()
+	exercise3()
 }
 
 func exercise1() {
@@ -31,4 +32,32 @@ func exercise2() {
 	}()
 	fmt.Println("\t", <-cs)
 	fmt.Printf("\t cs%T\n", cs)
+}
+
+func exercise3() {
+	/******
+	* Ex 3: use the range clause to print off values from channel
+	******/
+	fmt.Println("Ex 3: Range clause to pull values off of channel")
+	c := gen()
+	receive(c)
+
+	fmt.Println("\tabout to exit")
+}
+func receive(ch <-chan int) {
+	for val := range ch {
+		fmt.Println("\t", val)
+	}
+}
+
+func gen() <-chan int {
+	c := make(chan int)
+	go func(c chan<- int) {
+		for i := 0; i < 15; i++ {
+			c <- i
+		}
+		close(c)
+	}(c)
+
+	return c
 }
